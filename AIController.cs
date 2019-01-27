faa281144316f01b9d0dcdc3618f72896ecb4710
Preethi Venkataraman <31289330+preethiv98@users.noreply.github.com>
2019-01-26 21:33:39 -0500

    Add files via upload

--- AIController.cs
+++ AIController.cs
@@ -1,48 +1,102 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-public class AIController: MonoBehaviour
+using System.Collections;
+
+public class AIController : MonoBehaviour
 {
-     var target : Transform; //cant't be bothered to do any commments
- var moveSpeed = 3; 
- var rotationSpeed = 3; 
- var range : float=10f;
- var range2 : float=10f;
- var stop : float=0;
- var myTransform : Transform; 
- function Awake()
- {
-     myTransform = transform; 
- }
-  
- function Start()
- {
-      target = GameObject.FindWithTag("Player").transform; 
-  
- }
-  
- function Update () {
-     //rotate to look at the player
-     var distance = Vector3.Distance(myTransform.position, target.position);
-     if (distance<=range2 &&  distance>=range){
-     myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
-     Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed*Time.deltaTime);
-     }
-  
-  
-     else if(distance<=range && distance>stop){
-  
-     //move towards the player
-     myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
-     Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed*Time.deltaTime);
-     myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
-     }
-     else if (distance<=stop) {
-     myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
-     Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed*Time.deltaTime);
-     }
-  
- }
-    
+
+    private Animator animator;
+    private GameObject player;
+    private bool collidedWithPlayer;
+
+    void Awake()
+    {
+    player = GameObject.FindGameObjectWithTag("Player");
+     animator = GetComponent<Animator>();
+    }
+
+ 
+
+    void OnTriggerEnter(Collider other)
+
+    {
+
+        if (other.gameObject == player)
+
+        {
+
+            animator.SetBool("IsNearPlayer", true);
+
+        }
+
+        print("enter trigger with player");
+
+    }
+ 
+
+    void OnCollisionEnter(Collision other)
+
+    {
+
+        if (other.gameObject == player)
+
+        {
+
+            collidedWithPlayer = true;
+
+        }
+
+        print("enter collided with player");
+
+    }
+
+ 
+
+    void OnCollisionExit(Collision other)
+
+    {
+
+        if (other.gameObject == player)
+
+        {
+
+            collidedWithPlayer = false;
+
+        }
+
+        print("exit collided with player");
+
+    }
+
+ 
+
+    void OnTriggerExit(Collider other)
+
+    {
+
+        if (other.gameObject == player)
+
+        {
+
+            animator.SetBool("IsNearPlayer", false);
+
+        }
+
+        print("exit trigger with player");
+
+    }
+
+    void Attack()
+
+    {
+
+        if (collidedWithPlayer)
+
+        {
+
+            print("player has been hit");
+
+        }
+
+    }
+
 }
